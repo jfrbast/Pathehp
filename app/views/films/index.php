07704f6fdd965eb7c6b4ff8c8dff9bf4b744b290
etiennelb95 <etiennelb95@gmail.com>
2026-02-22 18:57:07 +0100

    Singe

--- app/views/films/index.php
+++ app/views/films/index.php
@@ -3,27 +3,37 @@
 <?php if (empty($films)): ?>
     <p>Aucun film pour le moment.</p>
 <?php else: ?>
-    <table>
-        <thead>
-        <tr>
-            <th>Titre</th>
-            <th>Durée (min)</th>
-            <th></th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($films as $film): ?>
-            <tr>
-                <td><?php echo \Security::e($film['title']); ?></td>
-                <td><?php echo (int)$film['duration']; ?></td>
-                <td>
-                    <a href="<?php echo BASE_URL; ?>?controller=film&action=show&id=<?php echo (int)$film['id']; ?>">
-                        Voir les séances
-                    </a>
-                </td>
-            </tr>
+    <div class="films-grid">
+        <?php foreach ($films as $film):
+            $title = $film['title'] ?? '';
+            if (!empty($film['poster_url'])) {
+                $posterSrc = $film['poster_url'];
+            } elseif (stripos($title, 'King Kong') !== false) {
+                $posterSrc = BASE_URL . 'assets/images/king-kong.png';
+            } elseif (stripos($title, 'Planète des Singes') !== false || stripos($title, 'Planete des Singes') !== false) {
+                $posterSrc = BASE_URL . 'assets/images/planete-des-singes.png';
+            } elseif (stripos($title, 'Madagascar') !== false && (stripos($title, 'Babouins') !== false || stripos($title, 'Opération') !== false)) {
+                $posterSrc = BASE_URL . 'assets/images/madagascar-operation-babouins.png';
+            } else {
+                $posterSrc = 'https://www.themoviedb.org/t/p/w600_and_h900_face/pFStqHTVUceciD9vineiNJ9WHpA.jpg';
+            }
+            ?>
+            <div class="film-card">
+                <div class="film-poster-wrapper">
+                    <img src="<?php echo \Security::e($posterSrc); ?>"
+                         alt="Affiche de <?php echo \Security::e($title); ?>">
+                </div>
+                <div class="film-card-body">
+                    <p class="film-card-title"><?php echo \Security::e($title); ?></p>
+                    <p class="film-card-meta"><?php echo (int)($film['duration'] ?? 0); ?> min</p>
+                    <div class="film-card-actions">
+                        <a href="<?php echo BASE_URL; ?>?controller=film&action=show&id=<?php echo (int)$film['id']; ?>">
+                            Voir les séances
+                        </a>
+                    </div>
+                </div>
+            </div>
         <?php endforeach; ?>
-        </tbody>
-    </table>
+    </div>
 <?php endif; ?>
 
